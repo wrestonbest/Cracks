96b8e9270e75dc8827748db976cbb46d95790fb0
wrestonbest <wreston7@hotmail.com>
2025-09-07 01:57:56 +0300

    Create loader.lua

---
+++ Kurd Hub/loader.lua
@@ -0,0 +1,26 @@
+local link = "https://raw.githubusercontent.com/wrestonbest/Cracks/refs/heads/main/Kurd%20Hub/"
+local files = {
+    "BaseEsp.lua",
+    "FinderScript.lua",
+    "PetFinder.lua",
+    "PetFinder1.lua",
+    "Spp2.lua",
+    "kickSah.lua",
+    "lock.lua",
+    "part.lua",
+    "playerEsp.lua",
+    "spp.lua"
+}
+
+for i, v in next, files do
+wait(1)
+loadstring(game:HttpGet(link..v))()
+end
+
+local startergui= game:GetService("StarterGui")
+
+startergui:SetCore("SendNotification", {
+    Title = "LEAKEDDDD",      
+    Text = "Script Leaked by Wreston, Shadow, Matemahirbaz, Drogba", 
+    Duration = 6 
+})
